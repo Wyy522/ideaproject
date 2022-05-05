be034277f0abba58c1398d69286e5199eac6117b
Wyy522 <523306566@qq.com>
2022-05-05 08:33:07 +0800

    1

---
+++ ProcessDispatcher/src/java/FCFS.java
@@ -0,0 +1,40 @@
+package java;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class FCFS {
+    public static void main(String[] args) {
+
+        // 对进程的到达时间排序
+        Arrays.sort(Process.processes, Comparator.comparingInt(e -> e.arriveTimePoint));
+
+        // 队列模拟程序调度
+        LinkedList<Process> queue = new LinkedList<>();
+
+        // 数组转换为队列
+        queue.addAll(Arrays.asList(Process.processes));
+
+        // 总时间
+        int timePoint = 0;
+
+        while(!queue.isEmpty()) {
+            // 出队
+            Process process = queue.poll();
+
+            // 进程未到达
+            if(timePoint < process.arriveTimePoint) {
+                timePoint = process.arriveTimePoint;
+            }
+
+            // 执行完该进程的时间
+            timePoint += process.serveTime;
+            // 统计
+            process.statistic(timePoint);
+        }
+
+        Process.printResult();
+
+    }
+}
