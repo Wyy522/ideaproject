e4c46f8f167a3896d05b1bef438a7794f03a1a1e
Wyy522 <523306566@qq.com>
2022-05-17 22:51:37 +0800

    1

---
+++ springboot-vue-test/src/main/java/com/example/springbootvuetest/SpringbootVueTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvuetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueTestApplication.class, args);
+    }
+
+}
---
+++ springboot-vue-test/src/test/java/com/example/springbootvuetest/SpringbootVueTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvuetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
